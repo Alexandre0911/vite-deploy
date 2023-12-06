fa7a9bc54a349f79c0ac5c92432f7b9447b2a05f
Alex <goncalo.alexandre.0911@gmail.com>
2023-12-06 05:25:12 +0000

    Fixing Assets Path

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,21 +1,21 @@
 export default [
     {
         title: "Project N.1",
-        imgUrl: "/assets/dev-portfolio.jpg",
+        imgUrl: "./assets/dev-portfolio.jpg",
         stack: ["TBD", "TBD", "TBD"],
         link: ""
     },
 
     {
         title: "Developer Portfolio",
-        imgUrl: "/assets/dev-portfolio.jpg",
+        imgUrl: "./assets/dev-portfolio.jpg",
         stack: ["React", "Tailwind", "Vite"],
-        link: "https://alexandre0911.github.io/"
+        link: "https://alexandre0911.github.io/vite-deploy/"
     },
 
     {
         title: "Project N.3",
-        imgUrl: "/assets/dev-portfolio.jpg",
+        imgUrl: "./assets/dev-portfolio.jpg",
         stack: ["TBD", "TBD", "TBD"],
         link: ""
     }
